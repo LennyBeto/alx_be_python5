ed8e3b27e7012154647b4cdb166180d22dcf352a
LennyBeto <lennybeto95@gmail.com>
2024-09-15 21:40:46 +0300

    I have added basic_operations.py file

--- basic_operations.py
+++ basic_operations.py
@@ -10,6 +10,6 @@ multiplication_result = number1 * number2
 # Print the results in the specified format
 print("Addition of", number1, "and", number2, "is", addition_result)
 print("Subtraction of", number1, "and", number2, "is", subtraction_result)
-print("Multiplication of", number1, "and", number2, "is" multiplication_result)
+print("Multiplication of", number1, "and", number2, "is", multiplication_result)
 
 
