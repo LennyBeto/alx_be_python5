5fb51a8cb7ba0028827cd93de8eeecedd9a6afbf
LennyBeto <lennybeto95@gmail.com>
2024-09-15 21:30:19 +0300

    I have added a basic_operations.py file

---
+++ basic_operations.py
@@ -0,0 +1,15 @@
+# Define number1 and number2 with values 10 and 5 respectively
+number1 = 10
+number2 = 5
+# Calculate the sum
+addition_result = number1 + number2
+# Calculate the difference (subtracting number2 from number1)
+subtraction_result = number1 - number2
+# Calculate the product
+multiplication_result = number1 * number2
+# Print the results in the specified format
+print("Addition of", number1, "and", number2, "is", addition_result)
+print("Subtraction of", number1, "and", number2, "is", subtraction_result)
+print("Multiplication of", number1, "and", number2, "is" multiplication_result)
+
+
